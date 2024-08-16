c47fc601c7c46134661aa2bee74e47339aafb441
Andrea Melissa Alvarez Herrera <andrea.alvarez@senprende.hn>
2024-08-16 16:35:03 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+
+
+let nombre = "ANDREA"; 
+
+console.log(`Hola mundo, mi nombre es ${nombre}, este es mi primer mensaje desde Node.js`);
+
+let edad = 23
+
+if(edad > 18){
+    console.log("Eres mayor de edad");
+} else {
+    console.log("No eres mayor de edad");
+}
+
+function sumar(a, b) {
+    return a +b;
+}
+
+console.log(sumar(13,20));
